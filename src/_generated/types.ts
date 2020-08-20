eb6c1e1c7c341d61e66abc953719d58a027c0fda
fjguipen <fjguipen@gmail.com>
2020-08-20 12:15:58 +0200

    generated types

---
+++ src/_generated/types.ts
@@ -0,0 +1,48 @@
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  lesson?: Maybe<Scalars['String']>;
+  level?: Maybe<Scalars['String']>;
+  user?: Maybe<User>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  login?: Maybe<CurrentUser>;
+  logout?: Maybe<CurrentUser>;
+};
+
+
+export type MutationLoginArgs = {
+  input?: Maybe<LoginInput>;
+};
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['ID'];
+  name?: Maybe<Scalars['String']>;
+  email?: Maybe<Scalars['String']>;
+  password?: Maybe<Scalars['String']>;
+};
+
+export type CurrentUser = {
+  __typename?: 'CurrentUser';
+  id: Scalars['ID'];
+  name?: Maybe<Scalars['String']>;
+  email?: Maybe<Scalars['String']>;
+};
+
+export type LoginInput = {
+  username?: Maybe<Scalars['String']>;
+  password?: Maybe<Scalars['String']>;
+};
